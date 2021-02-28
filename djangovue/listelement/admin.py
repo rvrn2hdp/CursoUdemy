16d2adc5c8c620a88c06d885c1866cef6f65cea1
rvrn2hdp <rvrn2hdp@gmail.com>
2021-02-28 12:16:33 -0300

    registrando modelos en sitio admin

--- djangovue/listelement/admin.py
+++ djangovue/listelement/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from .models import *
+
 # Register your models here.
+
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+
+class ElementAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+
+admin.site.register(Types, TypeAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Element, ElementAdmin)
