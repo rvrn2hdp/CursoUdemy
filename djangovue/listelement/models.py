62480c70e8fb4863dafac8534613dee9f23d9cc3
rvrn2hdp <rvrn2hdp@gmail.com>
2021-02-28 11:11:44 -0300

    modelos creados

--- djangovue/listelement/models.py
+++ djangovue/listelement/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    url_clean = models.CharField(max_length=255)
+
+
+class Types(models.Model):
+    title = models.CharField(max_length=255)
+    url_clean = models.CharField(max_length=255)
+
+
+class Element(models.Model):
+    title = models.CharField(max_length=255)
+    url_clean = models.CharField(max_length=255)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    types = models.ForeignKey(types, on_delete=models.CASCADE)
+
