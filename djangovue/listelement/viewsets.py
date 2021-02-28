0509f6408ac11269384970307d817f4a1aa7034f
rvrn2hdp <rvrn2hdp@gmail.com>
2021-02-28 17:36:55 -0300

    metodos personalizados

--- djangovue/listelement/viewsets.py
+++ djangovue/listelement/viewsets.py
@@ -1,7 +1,10 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .models import *
 from .serializer import *
+
 
 class ElementViewSet(viewsets.ModelViewSet):
     queryset = Element.objects.all()
@@ -12,7 +15,19 @@ class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
+    @action(detail=True, methods=['get'])
+    def elements(self, request, pk=None):
+        queryset = Element.objects.filter(category_id=pk)
+        serializer = ElementSerializer(queryset, many=True)
+        return Response(serializer.data)
+
 
 class TypesViewSet(viewsets.ModelViewSet):
     queryset = Types.objects.all()
     serializer_class = TypesSerializer
+
+    @action(detail=True, methods=['get'])
+    def elements(self, request, pk=None):
+        queryset = Element.objects.filter(types_id=pk)
+        serializer = ElementSerializer(queryset, many=True)
+        return Response(serializer.data)
