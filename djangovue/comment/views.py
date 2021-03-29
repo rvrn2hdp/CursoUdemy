aa95693780bb4964a83c24f7ed8e35245514b168
rvrn2hdp <rvrn2hdp@gmail.com>
2021-03-28 21:55:25 -0300

    comment update y redirect añadido

--- djangovue/comment/views.py
+++ djangovue/comment/views.py
@@ -1,23 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
 from .models import Comment
 from .forms import CommentForm
 
 # Create your views here.
 
+
 def index(request):
     comments = Comment.objects.all()
-    return render(request,'index.html', {'comments':comments})
+    return render(request, 'index.html', {'comments': comments})
+
 
 def add(request):
     # si el metodo es Post
     if request.method == 'POST':
-        # se crea el formulario 
+        # se crea el formulario
         form = CommentForm(request.POST)
         # si el formulario es valido
         if form.is_valid():
             form.save()
+            return redirect('index')
     else:
         form = CommentForm()
-    
-    return render(request,'add.html', {'form':form})
+
+    return render(request, 'add.html', {'form': form})
+
+
+def update(request, pk):
+
+    com = get_object_or_404(Comment, pk=pk)
+    # si el metodo es Post
+    if request.method == 'POST':
+        # se crea el formulario
+        form = CommentForm(request.POST, instance=com)
+        # si el formulario es valido
+        if form.is_valid():
+            form.save()
+            return redirect('update', pk=pk)
+    else:
+        form = CommentForm(instance=com)
+
+    return render(request, 'update.html', {'form': form, 'com': com})
+
